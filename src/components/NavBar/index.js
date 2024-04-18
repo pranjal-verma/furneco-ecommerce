86a54e4fe3ef8b70e169e277a9cb4b2bd60248ab
Pranjal Verma <pranjal.verma@leadschool.in>
2024-04-18 22:40:13 +0530

    ADD: navbar completed

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import styles from "./index.module.scss";
 import Image from "next/image";
+import {
+  ShoppingCartOutlined,
+  HeartOutlined,
+  SearchOutlined,
+  UserAddOutlined,
+} from "@ant-design/icons";
 function Navbar() {
   return (
     <div className={styles.navbar}>
       <div className={styles.logo}>
         <Image
           src="/logos/main.svg"
-          width={34}
-          height={34}
+          width={50}
+          height={32}
           alt="Picture of the author"
         ></Image>
         <p>Furniro</p>
@@ -18,6 +24,20 @@ function Navbar() {
         <p>Shop</p>
         <p>About</p>
         <p>Contact</p>
+      </div>
+      <div className={styles.navbar_icons}>
+        <div className={styles.navbar_icon_container}>
+          <UserAddOutlined />
+        </div>
+        <div className={styles.navbar_icon_container}>
+          <SearchOutlined />
+        </div>
+        <div className={styles.navbar_icon_container}>
+          <HeartOutlined />
+        </div>
+        <div className={styles.navbar_icon_container}>
+          <ShoppingCartOutlined />
+        </div>
       </div>
       {/* Navbar */}
     </div>
