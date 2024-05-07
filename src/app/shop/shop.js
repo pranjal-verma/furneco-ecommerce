0deacbb01a384ac5101d2163c6493258f70d7439
Pranjal Verma <pranjal.verma150398@gmail.com>
2024-05-07 22:19:05 +0530

    ADD scaffoliding for shop page

--- src/app/shop/shop.js
+++ src/app/shop/shop.js
@@ -1,15 +1,78 @@
-import Banner from '@/components/Banner'
-import React from 'react'
-
+import Banner from "@/components/Banner";
+import React from "react";
+import styles from "./index.module.scss";
+import ProductInfoCard from "@/components/ProductInfoCard";
 function ShopPage() {
+  const products = getProducts();
   return (
     <>
-    <div>
-      <Banner></Banner>
-    </div>
-    
+      <div>
+        <Banner></Banner>
+        {/* Filter and pagination */}
+        <div className={styles.filter}></div>
+
+        {/* Product Cards section */}
+        <div className={styles.products_container}>
+          {products.map((prod, i) => {
+            return <ProductInfoCard key={i} imageUrl={prod.url} />;
+          })}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default ShopPage
+export default ShopPage;
+
+function getProducts() {
+  return [
+    {
+      url: "/couch.png",
+      title: "Sultherine",
+    },
+    {
+      url: "/dining.png",
+      title: "Grito",
+    },
+    {
+      url: "/banner.jpeg",
+      title: "Lolito",
+    },
+    {
+      url: "/dining.png",
+      title: "Sultherine",
+    },
+    {
+      url: "/dining.png",
+      title: "Grito",
+    },
+    {
+      url: "/banner.jpeg",
+      title: "Lolito",
+    },
+    {
+      url: "/bedroom.png",
+      title: "Sultherine",
+    },
+    {
+      url: "/couch.png",
+      title: "Grito",
+    },
+    {
+      url: "/dining.png",
+      title: "Grito",
+    },
+    {
+      url: "/banner.jpeg",
+      title: "Lolito",
+    },
+    {
+      url: "/bedroom.png",
+      title: "Sultherine",
+    },
+    {
+      url: "/couch.png",
+      title: "Grito",
+    },
+  ];
+}
